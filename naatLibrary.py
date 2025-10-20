9b8d6ce218313fcce70da10d4135c8718965d9bc
itshamzi-001 <70158489@student.uol.edu.pk>
2025-10-20 21:27:42 +0500

    Add files via upload

---
+++ naatLibrary.py
@@ -0,0 +1,4 @@
+naat = {
+    "first" : "https://www.youtube.com/watch?v=KRHg6yZw3r0&list=RDKRHg6yZw3r0&start_radio=1",
+    "second" : "https://www.youtube.com/watch?v=MCA6u5eCLok&list=RDMCA6u5eCLok&start_radio=1"
+}
